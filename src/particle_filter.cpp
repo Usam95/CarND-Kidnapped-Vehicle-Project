3e00ec479aef02f894384ae1902b6e72d6a37897
Usam95 <usam.sersultanov@gmail.com>
2021-07-25 20:59:29 +0200

    added initialization of particle filter

--- src/particle_filter.cpp
+++ src/particle_filter.cpp
@@ -15,11 +15,14 @@
 #include <random>
 #include <string>
 #include <vector>
-
+#include <cassert>
 #include "helper_functions.h"
 
 using std::string;
 using std::vector;
+using std::normal_distribution;
+
+
 
 void ParticleFilter::init(double x, double y, double theta, double std[]) {
   /**
@@ -30,7 +33,25 @@ void ParticleFilter::init(double x, double y, double theta, double std[]) {
    * NOTE: Consult particle_filter.h for more information about this method 
    *   (and others in this file).
    */
-  num_particles = 0;  // TODO: Set the number of particles
+  num_particles = 100;  // TODO: Set the number of particles
+  std::default_random_engine gen;
+
+  //assert(("std[] Array must contain three elements!", sizeof(std)/sizeof(*std) == 3));
+  normal_distribution<double> dist_x(x, std[0]);
+  normal_distribution<double> dist_y(y, std[1]);
+  normal_distribution<double> dist_theta(theta, std[2]);
+
+  for(int i=0; i<num_particles; i++){
+    Particle particle = Particle();
+    particle.x = dist_x(gen);
+    particle.y = dist_y(gen);
+    particle.theta = dist_theta(gen); 
+    particle.weight = 1; 
+
+    this->particles.push_back(particle);
+  }
+
+  std::cout << "The filter is initialized with 100 particles." << std::endl;
 
 }
 
